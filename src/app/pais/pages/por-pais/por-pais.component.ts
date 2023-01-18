b75b563bd08c96214f31bd0aecbbb7e9e21c942e
Jesus Ruben Ortiz Chavez <jorticha@emeal.nttdata.com>
2023-01-18 07:27:23 -0500

    Subiendo cambios para avanzar en mi laptop personal

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html'
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
 
-  constructor() { }
+  termino: string = '';
+  hayError: boolean = false;
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor(private paisService: PaisService) { }
+
+  buscar( termino: string){
+    this.hayError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais( this.termino ).subscribe( paises => {
+      console.log(paises);
+      this.paises = paises;
+    }, (err) => {
+      this.hayError = true;
+      this.paises = [];
+    });
   }
 
+  sugerencias(termino: string){
+    this.hayError = false;
+    //TODO: crear sugerencias
+  }
 }
--- src/app/pais/pais.module.ts
+++ src/app/pais/pais.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
+import { PaisTablaComponent } from './components/pais-tabla/pais-tabla.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
+
 
 
 
@@ -12,7 +18,9 @@ import { PorRegionComponent } from './pages/por-region/por-region.component';
     PorCapitalComponent,
     PorPaisComponent,
     VerPaisComponent,
-    PorRegionComponent
+    PorRegionComponent,
+    PaisTablaComponent,
+    PaisInputComponent
   ],
   exports: [
     PorCapitalComponent,
@@ -21,7 +29,9 @@ import { PorRegionComponent } from './pages/por-region/por-region.component';
     PorRegionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule
   ]
 })
 export class PaisModule { }
